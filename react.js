0df307634df030e7b4bd267d4f5dc13a9fb78f5e
Juan David Merchan Torres <juan1014166@gmail.com>
2023-05-27 09:28:38 -0500

    feat(react): Add base react eslint config

---
+++ react.js
@@ -0,0 +1,38 @@
+module.exports = {
+  extends: ['plugin:react/recommended', 'plugin:react/jsx-runtime'],
+  plugins: ['react'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/self-closing-comp': 'error',
+    'react/jsx-sort-props': [
+      'warn',
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      rules: {
+        'react/jsx-filename-extension': [
+          'error',
+          { extensions: ['.ts', '.tsx'] },
+        ],
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+};
